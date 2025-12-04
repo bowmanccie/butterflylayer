1bc09160beb08bf17b7acb4d12913c254fb9ea69
Jennifer Bowman <jenbowma@cisco.com>
2025-12-03 22:17:56 -0500

    Fixed mentoring header.

--- assets/js/blog-list.js
+++ assets/js/blog-list.js
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
       .filter(Boolean);
   }
 
-  fetch("/blog/posts.json?v=20251203220942")
+  fetch("/blog/posts.json?v=20251203221751")
     .then((res) => {
       if (!res.ok) throw new Error("Unable to load posts.json");
       return res.json();
@@ -91,7 +91,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const thumbSrc =
           post.thumbnail ||
-          "/assets/img/ui/hero-butterfly-wow.svg?v=20251203220942";
+          "/assets/img/ui/hero-butterfly-wow.svg?v=20251203221751";
 
         const card = document.createElement("article");
         card.className = "pub-item thumb-card";
