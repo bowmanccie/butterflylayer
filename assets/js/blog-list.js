02275fce022768d20f8e5eeb9b06c18ec0dc9553
Jennifer Bowman <jenbowma@cisco.com>
2025-12-03 22:23:28 -0500

    Hide quote bubble on small screens

--- assets/js/blog-list.js
+++ assets/js/blog-list.js
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
       .filter(Boolean);
   }
 
-  fetch("/blog/posts.json?v=20251203221751")
+  fetch("/blog/posts.json?v=20251203222324")
     .then((res) => {
       if (!res.ok) throw new Error("Unable to load posts.json");
       return res.json();
@@ -91,7 +91,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const thumbSrc =
           post.thumbnail ||
-          "/assets/img/ui/hero-butterfly-wow.svg?v=20251203221751";
+          "/assets/img/ui/hero-butterfly-wow.svg?v=20251203222324";
 
         const card = document.createElement("article");
         card.className = "pub-item thumb-card";
