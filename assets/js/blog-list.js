c1b8de3ce8cfd0616f03b791b1ebb17a10a253b0
Jennifer Bowman <jenbowma@cisco.com>
2025-12-07 00:37:39 -0500

    Publish updates to Butterfly Layer

--- assets/js/blog-list.js
+++ assets/js/blog-list.js
@@ -74,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
       .filter(Boolean);
   }
 
-  fetch("/blog/posts.json?v=20251205173512")
+  fetch("/blog/posts.json?v=20251207003735")
     .then((res) => {
       if (!res.ok) throw new Error("Unable to load posts.json");
       return res.json();
@@ -113,7 +113,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const thumbSrc =
           post.thumbnail ||
-          "/assets/img/ui/hero-butterfly-wow.svg?v=20251205173512";
+          "/assets/img/ui/hero-butterfly-wow.svg?v=20251207003735";
 
         const card = document.createElement("article");
         card.className = "pub-item thumb-card";
